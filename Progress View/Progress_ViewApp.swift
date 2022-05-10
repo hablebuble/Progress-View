ce9465da11f00d17d2213213475ed046c2511acf
Alexey Strelnikov <strelkov@me.com>
2022-05-10 11:07:00 +0300

    Initial Commit

---
+++ Progress View/Progress_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Progress_ViewApp.swift
+//  Progress View
+//
+//  Created by Alexey Strelnikov on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Progress_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
